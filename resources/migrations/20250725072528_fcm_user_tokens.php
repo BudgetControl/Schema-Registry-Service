0adaf1c40d1c716b4452b7f10a6e99f614cbebf5
Marco De Felice <marco.defelice@budgetcontrol.cloud>
2025-07-25 09:47:15 +0200

    fix migrations

---
+++ resources/migrations/20250725072528_fcm_user_tokens.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class FcmUserTokens extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function up(): void
+    {
+        $table = $this->table('fcm_tokens');
+        $table
+            ->addColumn('user_id', 'integer', ['null' => false])
+            ->addColumn('token', 'text', ['null' => false])
+            ->addColumn('device_info', 'text', ['null' => true])
+            ->addColumn('created_at', 'datetime', [
+            'default' => 'CURRENT_TIMESTAMP',
+            'null' => false
+            ])
+            ->addColumn('updated_at', 'datetime', [
+            'default' => 'CURRENT_TIMESTAMP',
+            'update' => 'CURRENT_TIMESTAMP',
+            'null' => false
+            ])
+            ->addIndex(['token'], ['unique' => true])
+            ->addForeignKey('user_id', 'users', 'id', [
+            'delete' => 'CASCADE',
+            'update' => 'NO_ACTION'
+            ])
+            ->create();
+    }
+
+    public function down(): void
+    {
+        $this->table('fcm_tokens')->drop()->save();
+    }
+
+}
